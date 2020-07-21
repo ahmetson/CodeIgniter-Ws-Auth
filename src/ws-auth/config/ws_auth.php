81952ca1b8abb10cd6b25a05c15d23d9e37e30b6
Medet Ahmetson Atabayev <admin@blocklords.io>
2020-07-21 07:35:12 +0700

    Update configuration
    1. use environment variable for site title
    2. use environment variable for admin email
    3. extend user cookies every time when logs inset to true

--- src/ws-auth/config/ws_auth.php
+++ src/ws-auth/config/ws_auth.php
@@ -127,20 +127,20 @@ $config['argon2_admin_params']		= [
  | The library will fail for empty password or password size above 4096 bytes.
  | This is an arbitrary (long) value to protect against DOS attack.
  */
-$config['site_title']                 = "Example.com";       // Site Title, example.com
-$config['admin_email']                = "admin@example.com"; // Admin Email, admin@example.com
+$config['site_title']                 = isset ( $_ENV [ 'SITE_TITLE' ] ) ? $_ENV [ 'SITE_TITLE' ] : 'Example.com';       // Site Title, example.com
+$config['admin_email']                = isset ( $_ENV [ 'SITE_TITLE' ] ) ? $_ENV [ 'SITE_TITLE' ] : 'admin@example.com'; // Admin Email, admin@example.com
 $config['default_group']              = 'members';           // Default group, use name
 $config['admin_group']                = 'admin';             // Default administrators group, use name
 $config['identity']                   = 'email';             /* You can use any unique column in your table as identity column.
 															    The values in this column, alongside password, will be used for login purposes
 															    IMPORTANT: If you are changing it from the default (email),
 															    		   update the UNIQUE constraint in your DB */
-$config['min_password_length']        = 8;                   // Minimum Required Length of Password (not enforced by lib - see note above)
+$config['min_password_length']        = 6;                   // Minimum Required Length of Password (not enforced by lib - see note above)
 $config['email_activation']           = FALSE;               // Email Activation for registration
 $config['manual_activation']          = FALSE;               // Manual Activation for registration
 $config['remember_users']             = TRUE;                // Allow users to be remembered and enable auto-login
-$config['user_expire']                = 86500;               // How long to remember the user (seconds). Set to zero for no expiration - see sess_expiration in CodeIgniter Session Config for session expiration
-$config['user_extend_on_login']       = FALSE;               // Extend the users cookies every time they auto-login
+$config['user_expire']                = 604800;              // How long to remember the user (seconds). Set to zero for no expiration - see sess_expiration in CodeIgniter Session Config for session expiration
+$config['user_extend_on_login']       = TRUE;                // Extend the users cookies every time they auto-login
 $config['track_login_attempts']       = TRUE;                // Track the number of failed login attempts for each user or ip.
 $config['track_login_ip_address']     = TRUE;                // Track login attempts by IP Address, if FALSE will track based on identity. (Default: TRUE)
 $config['maximum_login_attempts']     = 3;                   // The maximum number of failed login attempts.
